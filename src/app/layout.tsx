0e863f68c5c1aebc3dc8843ded7761c147ebbed8
Suriya Parvin <dev.suriyaparvin@gmail.com>
2025-11-26 18:55:46 +0530

    project setup

---
+++ src/app/layout.tsx
@@ -0,0 +1,14 @@
+import { ReactNode } from "react";
+import "./globals.css";
+
+export default function RootLayout({
+	children,
+}: Readonly<{
+	children: ReactNode;
+}>) {
+	return (
+		<html lang="en">
+			<body>{children}</body>
+		</html>
+	);
+}
